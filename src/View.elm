8947aa354ef9fc9e074f8d4eeefc6f7f2fac1283
Jack Lucky <jack.lucky.iv@gmail.com>
2017-03-18 14:16:07 -0700

    Create player list

--- src/View.elm
+++ src/View.elm
@@ -1,11 +1,57 @@
 module View exposing (view)
 
-import Html exposing (Html, div, text)
+import Html exposing (..)
+import Html.Attributes exposing (class)
 import Msgs exposing (Msg)
-import Models exposing (Model)
+import Models exposing (Model, Player)
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ text model ]
+        [ nav
+        , playerTable model.players
+        ]
+
+
+nav : Html Msg
+nav =
+    div [ class "clearfix mb2 white bg-black" ]
+        [ div [ class "left p2" ]
+            [ text "Nav" ]
+        ]
+
+
+playerTable : List Player -> Html Msg
+playerTable players =
+    div [ class "p2" ]
+        [ table []
+            [ thead [] [ headerRow ]
+            , tbody [] (playerRows players)
+            ]
+        ]
+
+
+headerRow : Html Msg
+headerRow =
+    tr []
+        [ th [] [ text "ID" ]
+        , th [] [ text "Name" ]
+        , th [] [ text "Level" ]
+        , th [] [ text "Actions" ]
+        ]
+
+
+playerRows : List Player -> List (Html Msg)
+playerRows players =
+    List.map playerRow players
+
+
+playerRow : Player -> Html Msg
+playerRow player =
+    tr []
+        [ td [] [ text player.id ]
+        , td [] [ text player.name ]
+        , td [] [ text (toString player.level) ]
+        , td [] []
+        ]
