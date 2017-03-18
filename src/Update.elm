d49ebdd8cc61e1c7e619f065af4ed0c2adc9aef6
Jack Lucky <jack.lucky.iv@gmail.com>
2017-03-18 15:04:04 -0700

    Handle OnFetchPlayers

--- src/Update.elm
+++ src/Update.elm
@@ -7,5 +7,5 @@ import Msgs exposing (Msg(..))
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        OnFetchPlayers response ->
+            ( { model | players = response }, Cmd.none )
