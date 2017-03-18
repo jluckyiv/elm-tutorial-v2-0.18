684ed8f6e4c58abba747993f32eed0f27718261c
Jack Lucky <jack.lucky.iv@gmail.com>
2017-03-18 14:02:32 -0700

    Clean up model import

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (..)
 
 import Html exposing (program)
 import Msgs exposing (Msg)
-import Models exposing (Model)
+import Models exposing (Model, initialModel)
 import Subscriptions exposing (subscriptions)
 import Update exposing (update)
 import View exposing (view)
@@ -10,7 +10,7 @@ import View exposing (view)
 
 init : ( Model, Cmd Msg )
 init =
-    ( Models.initialModel, Cmd.none )
+    ( initialModel, Cmd.none )
 
 
 main : Program Never Model Msg
