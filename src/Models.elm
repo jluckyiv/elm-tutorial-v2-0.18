1204be0270e28e73d244597cea1fa733ab0baf35
Jack Lucky <jack.lucky.iv@gmail.com>
2017-03-18 14:03:17 -0700

    Create Player model

--- src/Models.elm
+++ src/Models.elm
@@ -1,12 +1,29 @@
-module Models exposing (Model, initialModel)
+module Models exposing (Model, Player, initialModel)
 
 import Msgs exposing (Msg)
 
 
 type alias Model =
+    { players : List Player
+    }
+
+
+type alias PlayerId =
     String
+
+
+type alias Player =
+    { id : PlayerId
+    , name : String
+    , level : Int
+    }
 
 
 initialModel : Model
 initialModel =
-    "Hello"
+    { players =
+        [ Player "1" "Larry" 0
+        , Player "2" "Moe" 0
+        , Player "3" "Curly" 0
+        ]
+    }
